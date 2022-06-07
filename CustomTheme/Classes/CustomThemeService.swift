4d88975b7ea38018f32997f8cf911381f49d5b26
liangdahong <hi@liangdahong.com>
2022-06-07 23:51:37 +0800

    feat: init

--- CustomTheme/Classes/CustomThemeService.swift
+++ CustomTheme/Classes/CustomThemeService.swift
@@ -1,6 +1,24 @@
+//    MIT License
 //
-//  Created by liangdahong on 2022/5/23.
+//    Copyright (c) 2022 梁大红 https://liangdahong.com
 //
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
 
 import UIKit
 
@@ -25,14 +43,15 @@ public extension CustomThemeService {
 
 extension CustomThemeService {
     
-    func config<View: AnyObject>(_ view: View, block: @escaping ((_ view: View) -> ()))  {
+    @discardableResult
+    func config<View: AnyObject, T>(_ view: View, block: @escaping ((_ view: View) throws -> T)) rethrows -> T {
         let weakWrapper = WeakWrapper(value: view as AnyObject, block: { view in
             if let tmpView = view as? View {
-                block(tmpView)
+                _ = try? block(tmpView)
             }
         })
         weakWrappers.append(weakWrapper)
-        block(view)
+        return try block(view)
     }
 }
 
